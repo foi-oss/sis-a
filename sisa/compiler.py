cd9e539e9d90f62df25b51b2bc9b101607a0d9da
Josip Lisec <josip@jlx.cc>
2012-12-04 04:03:42 +0100

    Updated error messages and cleaned up compiler.compile

--- sisa/compiler.py
+++ sisa/compiler.py
@@ -42,7 +42,7 @@ def end(source, lbl):
   n = source[0]
   debug.log(" # %d: inserting implicit return instruction" % n)
   COMPILER_LABELS[lbl]['end'] = n
-  return bin(instructions.MNEM_DICT['ret'])[2:].zfill(5).ljust(16, '0')
+  return util.fixedbin(instructions.MNEM_DICT['ret'], 5).ljust(16, '0')
 
 @compiler_instr
 def log(source, *args):
@@ -56,76 +56,49 @@ def compile(f, fout):
 
   if len(COMPILER_ERRORS) != 0:
     return (False, COMPILER_ERRORS)
-  else:
-    with open(fout, 'wb') as out:
-      # 1. remove whitespace
-      pps = [line for line in parsed_source if len(line) != 0]
 
-      # 2. find all label markers and calculate their absolute positions
-      abspos = {}
-      for (n, line) in enumerate(pps):
-        if line[0] == '.':
-          label_id = line.replace('.', '')
-          abspos[label_id] = n - len(abspos.keys())
-          pps[n] = None
-          debug.log(" # found marker for label '%s' in line %d" % (label_id, n))
+  # 1. remove whitespace
+  pps = [line for line in parsed_source if len(line) != 0]
 
-      pps = [line for line in pps if line != None]
+  # 2. find all label markers and calculate their absolute positions
+  abspos = {}
+  for (n, line) in enumerate(pps):
+    if line[0] == '.':
+      label_id = line.replace('.', '')
+      abspos[label_id] = n - len(abspos.keys())
+      pps[n] = None
+      debug.log(" # %d: found marker for label '%s'" % (n, label_id))
 
-      # replace label markers with actual locations and $! with current position
-      for (n, line) in enumerate(pps):
-        pos = line.find('%')
-        replacement = None
+  pps = [line for line in pps if line != None]
 
-        if pos != -1:
-          label_id = line[pos:line.rfind('%')+1].replace('%', '')
-          replacement = abspos[label_id]
-          debug.log(" # inserting label address lbl%s -> %d" % (label_id, replacement))
-        else:
-          pos = line.find('!')
-          if pos != -1:
-            debug.log(" # inserting current location %d" % n)
-            replacement = n
+  # 3. replace label markers with actual locations
+  #    (and expand $! with current position)
+  for (n, line) in enumerate(pps):
+    pos = line.find('%')
+  
+    if pos != -1:
+      label_id = line[pos:line.rfind('%')+1].replace('%', '')
+      replacement = abspos[label_id]
+      pps[n] = (line[0:pos] + util.fixedbin(replacement, 8)).ljust(16, '0')
+      debug.log("%3d: inserting label address lbl%s -> %d" % (n, label_id, replacement))
+  
+    pos = line.find('!')
+    if pos != -1:
+      debug.log("%3d: inserting current location" % n)
+      pps[n] = (line[0:pos] + util.fixedbin(n, 8)).ljust(16, '0')
 
-        if replacement != None:
-          pps[n] = (line[0:pos] + bin(replacement)[2:].zfill(8)).ljust(16, '0')
+    debug.log("%3d: %s" % (n, pps[n]))
 
-        debug.log("%3d: %s" % (n, pps[n]))
+  with open(fout, 'wb') as out:
+    debug.log(" # Writing binary to '%s'..." % fout)
+    [out.write(raw_instr(line)) for line in pps]
 
-      [out.write(raw_instr(line)) for line in pps]
-
-    return (True, None)
+  return (True, None)
 
 def raw_instr(instr):
   if len(instr) == 0:
     return ""
   return ('\\x' + '\\x'.join(util.chunks(instr, 2))).decode('string_escape')
-
-def replace_addr(line, n):
-  pos = line.find('!')
-  if pos != -1:
-    debug.log(" # inserting current address (%d) to previous instruction" % n)
-    return line[0:pos] + bin(n)[2:].zfill(8)
-
-  pos = line.find('%')
-  if pos != -1:
-    label_id = int(line[pos:].replace('%', ''))
-    label = get_label(label_id)
-    if label == None:
-      error('address of unknown label was requested:', label_id)
-      return line
-
-    debug.log(' # inserting address of label:', label)
-    return line[0:pos] + bin(label['start'])[2:].zfill(8)
-
-  return line
-
-def get_label(id):
-  for label in COMPILER_LABELS:
-    if COMPILER_LABELS[label]['id'] == id:
-      return COMPILER_LABELS[label]
-  
-  return None
 
 def parse_line((n, line)):
   line = line.strip()
@@ -157,7 +130,7 @@ def parse_line((n, line)):
       ))
     return ''
 
-  return (bin(opcode)[2:].zfill(5) + ''.join(args)).ljust(16, '0')
+  return (util.fixedbin(opcode, 5) + ''.join(args)).ljust(16, '0')
 
 
 # $var_name => expands variable
